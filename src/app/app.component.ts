524c11fa447057d2993316722cb2a391f1a95d15
waryplumblum <edi_wachimiro1805@hotmail.com>
2022-02-09 13:54:09 -0700

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas'; // Todavía no lo usamos
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  @Input('entrada') entrada = "default";
+  
   title = 'prueba1';
+
+   
+  // tslint:disable-next-line:typedef
+  downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('htmlData')!;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = { background: 'white', scale: 2 };
+    
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toDateString()}.pdf`);
+    }); 
+  }
+  
 }
